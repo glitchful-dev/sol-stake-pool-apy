162ff47b61e42aa7b03bda91525a02ee43fa7baa
janlegner <jan.legner@gmail.com>
2023-02-18 17:53:23 +0100

    db scripts

---
+++ prune-prices.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -x
+
+function prune_db {
+    db_file="$1"
+
+    if [[ -z $db_file ]]
+    then
+        echo "Missing arguments, usage: prune_db <db-file>"
+        exit 1
+    fi
+    tmp_file=$(mktemp)
+    cp "$db_file" "$tmp_file"
+    awk -F , '{
+        current = $2 "," $3
+        if (previous != current) {
+            print $0
+        }
+        previous = current
+    }' "$tmp_file" > "$db_file"
+    rm "$tmp_file"
+}
+
+prune_db './db/jito.csv' Jito
+prune_db './db/lido.csv'
+prune_db './db/marinade.csv'
---
+++ update-prices.bash
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+set -x
+
+result_file="./result.tmp"
+now="$(date -Iseconds)"
+
+pnpm run start > "$result_file"
+epoch=$(ggrep -oP "^epoch \K[0-9.]*" "$result_file")
+
+if [[ -z $epoch ]]
+then
+    echo "Epoch not found in the result file!"
+    exit 1
+fi
+
+function update_db {
+    db_file="$1"
+    search_exp="$2"
+
+    if [[ -z $db_file ]] || [[ -z $search_exp ]]
+    then
+        echo "Missing arguments, usage: update_db <db-file> <search-exp>"
+        exit 1
+    fi
+
+    price=$(ggrep -oP "^$search_exp \K[0-9.]*" "$result_file")
+    if [[ -z $price ]]
+    then
+        echo "Price not found in the result file!"
+        return
+    fi
+
+    echo "Found price for $search_exp: $price"
+
+    echo "$now,$epoch,$price" >> "$db_file"
+}
+
+update_db './db/jito.csv' Jito
+update_db './db/lido.csv'
+update_db './db/marinade.csv'
+
+rm "$result_file"
