44db81f552bfe54f2441f12e6b5f6c80fee251da
Alexandru Stefan <alex@ndru.dev>
2023-09-08 11:07:35 +0300

    fix: save everstake/socean prices

--- update-prices.bash
+++ update-prices.bash
@@ -43,11 +43,11 @@ function update_db {
 
 update_db './db/laine.csv' Laine
 update_db './db/cogent.csv' Cogent
-#update_db './db/everstake.csv' Everstake
+update_db './db/everstake.csv' Everstake
 update_db './db/solblaze.csv' SolBlaze
 update_db './db/daopool.csv' DAOPool
 update_db './db/jpool.csv' JPool
-#update_db './db/socean.csv' Socean
+update_db './db/socean.csv' Socean
 update_db './db/jito.csv' Jito
 update_db './db/lido.csv' Lido
 update_db './db/marinade.csv' Marinade
